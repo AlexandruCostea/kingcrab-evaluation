f227bfedf9b3a0570a46529fdc06cecc94577048
Alexandru Costea <costeasigartaualexandru@protonmail.com>
2025-06-06 17:47:00 +0300

    Changed data preprocessor to split processed data in 100k element buckets

--- src/data_preprocessor/src/main.rs
+++ src/data_preprocessor/src/main.rs
@@ -4,9 +4,11 @@ use preprocessor::fen_eval_preprocessor::FenEvalPreprocessor;
 
 fn main() {
     let unprocessed_data_path = "/home/alexcostea/KingCrab-Evaluation/data/lichess_db_eval.jsonl.zst".to_string();
-    let processed_data_path = "/home/alexcostea/KingCrab-Evaluation/data/lichess_db_processed.jsonl.zst".to_string();
+    let processed_data_path = "/home/alexcostea/KingCrab-Evaluation/data/".to_string();
+    let file_name_base = "lichess_db_processed".to_string();
+    let extension = ".jsonl.zst".to_string();
 
-    let preprocessor = FenEvalPreprocessor::new(unprocessed_data_path, processed_data_path);
+    let preprocessor = FenEvalPreprocessor::new(unprocessed_data_path, processed_data_path, file_name_base, extension);
 
     if let Err(e) = preprocessor.run() {
         eprintln!("Error during preprocessing: {}", e);
