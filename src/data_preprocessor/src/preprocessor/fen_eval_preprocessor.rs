ff8dba7edee55169b3f93a8a5841a57c8a65bf0e
Alexandru Costea <costeasigartaualexandru@protonmail.com>
2025-06-11 00:11:35 +0300

    Reverted to original preprocessor format

--- src/data_preprocessor/src/preprocessor/fen_eval_preprocessor.rs
+++ src/data_preprocessor/src/preprocessor/fen_eval_preprocessor.rs
@@ -15,13 +15,11 @@ struct FenEval {
 pub struct FenEvalPreprocessor {
     input_path: String,
     output_path: String,
-    file_name_base: String,
-    extension: String,
 }
 
 impl FenEvalPreprocessor {
-    pub fn new(input_path: String, output_path: String, file_name_base: String, extension: String) -> Self {
-        Self { input_path, output_path, file_name_base, extension }
+    pub fn new(input_path: String, output_path: String) -> Self {
+        Self { input_path, output_path }
     }
 
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
@@ -29,15 +27,12 @@ impl FenEvalPreprocessor {
         let reader = Decoder::new(BufReader::new(input_file))?;
         let stream = Deserializer::from_reader(reader).into_iter::<EvalEntry>();
 
-
-        let mut output_file_name = format!("{}{}1{}", self.output_path, self.file_name_base, self.extension);
-        let mut output_file = File::create(&output_file_name)?;
-        let mut buf_writer = BufWriter::new(output_file);
+        let output_file = File::create(&self.output_path)?;
+        let buf_writer = BufWriter::new(output_file);
         let mut encoder = Encoder::new(buf_writer, 3)?;
 
-        let mut file_index = 1;
+
         let mut count = 0;
-        let mut total_count = 0;
 
         for result in stream {
             let entry = match result {
@@ -60,26 +55,13 @@ impl FenEvalPreprocessor {
             serde_json::to_writer(&mut encoder, &out)?;
             encoder.write_all(b"\n")?;
 
-            total_count += 1;
             count += 1;
-
-            if count >= 100_000 {
-                encoder.flush()?;
-                file_index += 1;
-                output_file_name = format!("{}{}{}{}", self.output_path, self.file_name_base, file_index, self.extension);
-                output_file = File::create(&output_file_name)?;
-
-                buf_writer = BufWriter::new(output_file);
-                encoder = Encoder::new(buf_writer, 3)?;
-
-                count = 0;
-            }
         }
 
         encoder.finish()?;
 
 
-        println!("Wrote {} FEN/eval pairs.", total_count);
+        println!("Wrote {} FEN/eval pairs.", count);
         Ok(())
     }
 }
